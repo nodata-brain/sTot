475a3264d20a4718131a985a3d64d2acc39e94b4
nodata <nodata.eng@gmail.com>
2020-10-17 22:31:48 +0900

    ✨ add pkg stot

--- cmd/main.go
+++ cmd/main.go
@@ -1,87 +1,22 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
+	"github.com/gordonklaus/portaudio"
 	"log"
-	"os"
-
-	speech "cloud.google.com/go/speech/apiv1"
-	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+	//"sTot/pkg/googleapi"
 )
 
 func main() {
-	ctx := context.Background()
-
-	client, err := speech.NewClient(ctx)
+	err := portaudio.Initialize()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Initialize error : %s", err)
+		return
 	}
-	stream, err := client.StreamingRecognize(ctx)
+	p, err := portaudio.OpenDefaultStream(4, 0, 44100.0, 256, 1)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Open Stream error : %s", err)
+		return
 	}
-	// Send the initial configuration message.
-	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
-			StreamingConfig: &speechpb.StreamingRecognitionConfig{
-				Config: &speechpb.RecognitionConfig{
-					Encoding:        speechpb.RecognitionConfig_LINEAR16,
-					SampleRateHertz: 16000,
-					LanguageCode:    "en-US",
-				},
-			},
-		},
-	}); err != nil {
-		log.Fatal(err)
-	}
-
-	go func() {
-		// Pipe stdin to the API.
-		buf := make([]byte, 1024)
-		for {
-			n, err := os.Stdin.Read(buf)
-			if n > 0 {
-				if err := stream.Send(&speechpb.StreamingRecognizeRequest{
-					StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
-						AudioContent: buf[:n],
-					},
-				}); err != nil {
-					log.Printf("Could not send audio: %v", err)
-				}
-			}
-			if err == io.EOF {
-				// Nothing else to pipe, close the stream.
-				if err := stream.CloseSend(); err != nil {
-					log.Fatalf("Could not close stream: %v", err)
-				}
-				return
-			}
-			if err != nil {
-				log.Printf("Could not read from stdin: %v", err)
-				continue
-			}
-		}
-	}()
-
-	for {
-		resp, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Cannot stream results: %v", err)
-		}
-		if err := resp.Error; err != nil {
-			// Workaround while the API doesn't give a more informative error.
-			if err.Code == 3 || err.Code == 11 {
-				log.Print("WARNING: Speech recognition request exceeded limit of 60 seconds.")
-			}
-			log.Fatalf("Could not recognize: %v", err)
-		}
-		for _, result := range resp.Results {
-			fmt.Printf("Result: %+v\n", result)
-		}
-	}
+	log.Printf("%v", p)
+	//stot.New()
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	cloud.google.com/go v0.69.1
+	github.com/gordonklaus/portaudio v0.0.0-20200911161147-bb74aa485641
 	google.golang.org/genproto v0.0.0-20201015140912-32ed001d685c
 )
